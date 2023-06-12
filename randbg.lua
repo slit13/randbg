63531e24274830e0b5ec12ce414d72ae605691f5
slit13 <slit13@mailbox.org>
2023-06-12 20:05:29 +0300

    idk, some stuff, still more left to do, mainly with file existence and error checking

--- randbg.lua
+++ randbg.lua
@@ -6,13 +6,18 @@ local File = ""
 local Dir = ""
 local Wildcard = ".*"
 
+function printErrorQuit(msg)
+	print(msg)
+	os.exit(1)
+end
+
+-- an integer representing the seed to be used in the random number generation
+-- change this function if you want
+RandomSeedValue = os.time()
+
 -- random int from a to b, both ends inclusive
 local function randomIntRange(a, b)
-	-- returns an integer representing the seed to be used in the random number generation
-	-- change this function if you want
-	local function randomSeedValue()
-		math.randomseed(os.time(), math.floor(os.time() / 2))
-	end
+	math.randomseed(math.floor(RandomSeedValue))
 
 	-- Swap values if b < a
 	if b < a then
@@ -27,11 +32,16 @@ end
 
 local function printHelp()
 	print(
-		arg[0] .. "\n"..
+		arg[0] .. "\n\n"..
+		"CLI Arguments:\n" ..
 		"\t-help              - print this screen\n" ..
 		"\t-file path_to_file - use a given file instead of randomly picking from a directory\n" ..
 		"\t-dir path_to_dir   - use a given directory instead of the value of BG_DIR\n" ..
-		"\twhatever_argument  - use the argument as a wildcard, the wildcard will be *whatever_argument* and the last such argument will be used as the wildcard"
+		"\t-seed number       - provide a custom seed for the random number generation, must be a number\n" ..
+		"\twhatever_argument  - use the argument as a wildcard, the wildcard will be *whatever_argument* and the last such argument will be used as the wildcard\n" ..
+		"Environment Variables:\n" ..
+		"\tBG_DIR            - path to the folder containing your background images\n" ..
+		"\tRANDBG_SILENT_FEH - 1 or true, makes the `feh` command completely silent including errors\n"
 	)
 end
 
@@ -52,6 +62,15 @@ local function parseArguments()
 		elseif arg[i] == "-dir" then
 			Dir = arg[i + 1]
 			skipNext = true
+		elseif arg[i] == "-seed" then
+			local seed = tonumber(arg[i + 1])
+			skipNext = true
+
+			if seed == nil then
+				printErrorQuit("RNG seed is not a number")
+			end
+
+			RandomSeedValue = seed
 		else
 			Wildcard = ".*" .. arg[i] .. ".*"
 		end
@@ -94,8 +113,7 @@ local function main()
 	if Dir == "" then
 		local bg_dir = os.getenv("BG_DIR")
 		if bg_dir == nil then
-			print("No folder available, use -dir path_to_folder or set BG_DIR")
-			os.exit(1)
+			printErrorQuit("No folder available, use -dir path_to_folder or set BG_DIR")
 		else
 			Dir = bg_dir
 		end
@@ -108,7 +126,15 @@ local function main()
 	assert(File ~= "", "ERROR: File is undefined")
 
 	local cmd = ("feh --no-fehbg --bg-max %s"):format(File)
-	os.execute(cmd)
+	local silentFeh = os.getenv("RANDBG_SILENT_FEH")
+	if silentFeh == "1" or silentFeh == "true" then
+		cmd = cmd .. " &> /dev/null"
+	end
+
+	local result = os.execute(cmd)
+	if not result then
+		print("ERROR: There was some error with feh, idk, open an issue on Github or something...")
+	end
 end
 
 main()
